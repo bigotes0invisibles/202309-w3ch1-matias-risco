42bbdcb3c1655f4698f075b3e403c6eec881c3d7
Matias Risco <matiasriscolagos@gmail.com>
2023-09-25 21:37:20 +0200

    Add show and hide with checkbox

--- js/index.js
+++ js/index.js
@@ -3,5 +3,19 @@ const formElement = document.querySelector("form");
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  validateForm();
+  validateForm(event.target);
 });
+
+formElement
+  .querySelector(".form-check-box")
+  .addEventListener("click", (event) => {
+    if (event.target.checked) {
+      formElement
+        .querySelectorAll(".date-time")
+        .forEach((element) => element.classList.remove("hidden"));
+    } else {
+      formElement
+        .querySelectorAll(".date-time")
+        .forEach((element) => element.classList.add("hidden"));
+    }
+  });
